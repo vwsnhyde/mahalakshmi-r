49c5df86b4dcaee93b8a80b38d0c706e849fc75b
vwsnhyde <31615062+vwsnhyde@users.noreply.github.com>
2017-11-16 12:55:33 +0530

    Add files via upload

---
+++ gas.py
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<wiringPi.h>
+#define GAS_PIN 19
+int main(void)
+{
+    wiringPiSetupGpio();
+    pinMode(GAS_PIN,INPUT);
+
+    while(1)
+    {
+        if(digitalRead(GAS_PIN)==LOW)
+        {
+            printf("GAS detected");
+
+            printf("\n");
+            }
+        else
+        {
+            printf("Cool environment");
+
+            printf("\n");
+            }
+        delay(2000);
+        }
+    return 0;
+    }
